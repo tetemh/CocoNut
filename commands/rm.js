628ed3a5c50308a3c6121441e4177a6618c9f8a5
Téo <76852029+tetemh@users.noreply.github.com>
2022-12-09 22:17:27 +0100

    tmp rm channel

--- commands/rm.js
+++ commands/rm.js
@@ -16,19 +16,25 @@ module.exports = {
 
     async run(bot, message, args, db) {
         if (typeof args._hoistedOptions[0] !== "undefined") {
-            if(args._hoistedOptions[0].value == "-R"){
-                let parent = message.guild.channels.cache.get(message.guild.channels.cache.get(message.channel.id).parentId)
-                // parent.children.forEach(channel => console.log(channel))
-                message.guild.channels.cache.forEach(channel => {
-                    if(channel.parentId == parent.id){
-                        channel.delete()
+            if (args._hoistedOptions[0].value == "-R") {
+                let parent = message.guild.channels.cache.get(
+                    message.guild.channels.cache.get(message.channel.id)
+                        .parentId
+                );
+                message.guild.channels.cache.forEach((channel) => {
+                    if (channel.parentId == parent.id) {
+                        channel.delete();
                     }
-                })
-                message.guild.channels.cache.get(message.guild.channels.cache.get(message.channel.id).parentId).delete()
+                });
+                message.guild.channels.cache
+                    .get(
+                        message.guild.channels.cache.get(message.channel.id)
+                            .parentId
+                    )
+                    .delete();
             }
         } else {
             message.guild.channels.cache.get(message.channel.id).delete();
-            await message.reply("Channel supprimer");
         }
     },
 };
